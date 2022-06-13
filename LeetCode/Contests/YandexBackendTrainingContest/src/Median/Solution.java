f8c4e9380a2d50f4cf244fce8f071d389b5096d8
Yaroslav Panteleev <yarik.panteleev@gmail.com>
2022-06-13 22:05:22 +0300

    Created another TL solution for Median

--- LeetCode/Contests/YandexBackendTrainingContest/src/Median/Solution.java
+++ LeetCode/Contests/YandexBackendTrainingContest/src/Median/Solution.java
@@ -6,46 +6,52 @@ public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int n = Integer.parseInt(console.nextLine());
-        char[] S = console.nextLine().toCharArray();
-        int[] L = new int[n + 1];
-        L[1] = -1;
+        char[] s = console.nextLine().toCharArray();
+
         int counter = 0;
-        if (S[0] == '0') {
+        int subCounter;
+        System.out.print(-1 + " ");
+        if (s[0] == '0') {
             counter--;
         } else {
             counter++;
         }
-        for (int r = 2; r <= n; r++) {
-            L[r] = -1;
-
-            if (S[r - 1] == '0') {
+        for (int l = 0, r = 1; r <= n - 1; r++, l = 0) {
+            if (s[r] == '0') {
                 counter--;
+                subCounter = counter;
+                while (l + 1 < r && !(subCounter < 0)) {
+                    if (s[l] == '0') {
+                        subCounter++;
+                    } else {
+                        subCounter--;
+                    }
+                    l++;
+                }
+
+                if (subCounter < 0) {
+                    System.out.print((l + 1) + " ");
+                } else {
+                    System.out.print(-1 + " ");
+                }
             } else {
                 counter++;
-            }
-            if ((counter < 0 && S[r - 1] == '0') || (counter > 0 && S[r - 1] == '1')) {
-                L[r] = 1;
-                continue;
-            }
-            int bufferCounter = counter;
-            for (int l = 1; l <= r - 2; l++) {
-                if (S[l - 1] == '0') {
-                    bufferCounter++;
-                } else {
-                    bufferCounter--;
+                subCounter = counter;
+                while (l + 1 < r && !(subCounter > 0)) {
+                    if (s[l] == '0') {
+                        subCounter++;
+                    } else {
+                        subCounter--;
+                    }
+                    l++;
                 }
-                if (bufferCounter == 0) {
-                    continue;
-                }
-                if (bufferCounter < 0 && S[r - 1] == '0' || bufferCounter > 0 && S[r - 1] == '1') {
-                    L[r] = l + 1;
-                    break;
-                }
-            }
-        }
 
-        for (int i = 1; i < n + 1; i++) {
-            System.out.print(L[i] + " ");
+                if (subCounter > 0) {
+                    System.out.print((l + 1) + " ");
+                } else {
+                    System.out.print(-1 + " ");
+                }
+            }
         }
     }
 }
