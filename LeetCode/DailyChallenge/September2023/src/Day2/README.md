53f7726b17cee1a498472bf6ea29592de96cb1e1
panteleev-ya <yarik.panteleev@gmail.com>
2023-09-16 14:52:28 +0300

    решил задачу дня 2

--- LeetCode/DailyChallenge/September2023/src/Day2/README.md
+++ LeetCode/DailyChallenge/September2023/src/Day2/README.md
@@ -1,2 +1,2 @@
-# 
-Решение задачи [](https://leetcode.com/problems/) c сайта **LeetCode** на языке **Java**
+# Extra Characters in a String
+Решение задачи [Extra Characters in a String](https://leetcode.com/problems/extra-characters-in-a-string) c сайта **LeetCode** на языке **Java**
--- LeetCode/DailyChallenge/September2023/src/Day2/Solution.java
+++ LeetCode/DailyChallenge/September2023/src/Day2/Solution.java
@@ -1,7 +1,37 @@
 package Day2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public static void main(String[] args) {
+        String s = "dwmodizxvvbosxxw";
+        // [d] {wmo} {diz} [x] {v} {v} [b] {o} [s] [x] [x] [w]
+        String[] dictionary = {
+                "cehy", "diz", "ds", "e",
+                "gu", "ksv", "kzbu", "lb",
+                "nuq", "o", "ox", "r",
+                "tskz", "txhe", "v", "wmo",
+        };
+        System.out.println(new Solution().minExtraChar(s, dictionary));
+    }
 
+    public int minExtraChar(String s, String[] dictionary) {
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, s.length() + 1);
+        dp[0] = 0;
+
+        Set<String> dictionarySet = new HashSet<>(Arrays.asList(dictionary));
+
+        for (int i = 1; i <= s.length(); i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int l = 1; l <= i; l++) {
+                if (dictionarySet.contains(s.substring(i - l, i))) {
+                    dp[i] = Math.min(dp[i], dp[i - l]);
+                }
+            }
+        }
+        return dp[s.length()];
     }
 }
